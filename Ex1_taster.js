4ca11a25b3a51968c098f39b76e66a8c1be71b09
Alan1234-wq <796737@manadulted.ac.uk>
2026-04-23 14:00:29 +0000

    Completed tasks 1

--- Ex1_taster.js
+++ Ex1_taster.js
@@ -36,8 +36,9 @@ console.log("-------------------------------------------");
 // console.log("I love " + food);
 
 // Write your code below:
-
-
+console.log("welcome to javascipt!");
+let user_name = "Tony"
+console.log("Hello " + user_name);
 
 // -----------------------------------------------------------
 // Task 2: Basic Arithmetic (The Calculator)
